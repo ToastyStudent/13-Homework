d8ba10701cb7c560fe6be945b1be0742aadcb629
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-06 16:31:34 -0500

    Added the collumns to the Tag.js model

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,22 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Definition of Collumns for the ProductTag Model, named "id," 
+    // and "tag_name" respectively
+    
+    // id is a primary key meaning its referenced as a foreign key elsewhere,
+    // is an integer, cannot be null, and autoincrements
+    
+    // tag_name is a string
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
