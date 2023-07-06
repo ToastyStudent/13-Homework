4222cb649b7d7158614efe9c2c336af51835927f
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-06 15:32:31 -0500

    Completed the index.js file within the models folder

--- models/index.js
+++ models/index.js
@@ -4,13 +4,33 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// The following code defines the relationship between each of the models
+// and one another
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
