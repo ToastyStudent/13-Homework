35e1a39a5347b0041d5b2bea20d9b7af68634dd9
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-06 16:38:45 -0500

    Added additional comments in the index.js file within the models folder

--- models/index.js
+++ models/index.js
@@ -7,25 +7,35 @@ const ProductTag = require('./ProductTag');
 // The following code defines the relationship between each of the models
 // and one another
 
-// Products belongsTo Category
+// Products belongsTo Category, meaning that the Product model 
+// has a foreign key in the form of "category_id" that references the
+// Category model's primary key of "id"
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
-// Categories have many Products
+// Categories have many Products, meaning that a single Category can have
+// multiple Products and that each one of said Products has a foreign key
+// in the form of "category_id" that references the Category model's
+// 
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag,) meaning that a
+// a single Product can have multiple Tags and that each one of said Tags
+// has a foreign key in the form of "product_id" that references the
+// Product model's primary key of "id"
 Product.belongsToMany(Tag, {
   through: ProductTag,
   // as: 'product_tags',
   foreignKey: 'product_id',
 });
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag,) meaning that a single Tag
+// can have multiple Products and that each one of said Products has a foreign
+// key in the form of "tag_id" that references the Tag model's primary key of "id"
 Tag.belongsToMany(Product, {
   through: ProductTag,
   // as: 'product_tags',
