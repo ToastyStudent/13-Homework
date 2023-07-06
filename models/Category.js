71dbbd4a3f0acc2efd33010dff1ca123877f06e4
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-06 15:28:10 -0500

    Added the collumns for the Category.js model

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,20 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Definition of Collumns for the Category Model, named "id" and
+    // "category_name" respectively
+    // id is a required field as a primary key, is an integer, cannot be null, and autoincrements
+    // category_name is a string and cannot be null
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
